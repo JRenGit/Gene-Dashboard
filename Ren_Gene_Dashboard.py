709b394a6ce77012a949267419b5e964473af852
JRenGit <justinrenprf@gmail.com>
2025-07-16 17:26:59 -0400

    Add files via upload
    Landing and nucleotide composition pages uploaded
    -Nucleotide page WIP

---
+++ Ren_Gene_Dashboard.py
@@ -0,0 +1,11 @@
+### Importing Libraries
+import pandas as pd
+import streamlit as st
+import altair as alt
+
+### Landing Page
+st.image(r"C:\Users\justi\Documents\Python Files\Bio_Streamlit\Gene Dashboard Banner.png")
+st.markdown("""
+# Welcome to a Multi-Function Gene Processing Application  
+### You can navigate to different tools using the sidebar menu
+""")
