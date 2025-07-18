9097d5a28adb433b191be720c8ea1e1d18e391ec
Justin Ren <justinrenprf@gmail.com>
2025-07-17 23:51:28 -0400

    Add files via upload
    Added counting function and fixed clean_fasta functionality

--- 1_Nucleotide_Composition.py
+++ 1_Nucleotide_Composition.py
@@ -13,3 +13,23 @@ TGAATGCTGATCCCCTGTGTGGGAGAAAAGAACTGAAGAAGCAGAAACCTCCATGCTCTGACAGTCCTAG
 """
 
 rawFASTA = st.text_area("", value=default_fasta, height=300)
+
+def cleanFASTA(rawFASTA: str) -> str:
+    lines = rawFASTA.strip().splitlines()
+    cleaned_lines: list[str] = []
+    
+    for line in lines[1:]:
+        line = line.strip()
+        if line:
+            cleaned_lines.append(line)
+            
+    cleanedFASTA: str = "".join(cleaned_lines)
+    return cleanedFASTA
+
+nucleotides = cleanFASTA(rawFASTA).upper()
+g_content: int = nucleotides.count('G')
+c_content: int = nucleotides.count('C')
+a_content: int = nucleotides.count('A')
+t_content: int = nucleotides.count('T')
+
+st.write(f"Your sequence is composed of {g_content} guanine, {c_content} cystine, {a_content} adenine, and {t_content} thymine.")
